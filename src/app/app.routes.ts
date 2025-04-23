d7d76e75cb72d484beaf7b9046a4d952418b2655
Naïké Dramé <naike.drame.dev@gmail.com>
2025-04-23 12:06:35 +0200

    wip routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,17 @@ import { HomeComponent } from './presentation/home/home.component';
 import { OverviewComponent } from './presentation/dashboard/overview.component';
 import { EventListComponent } from './presentation/events/event-list/event-list.component';
 import { RoomGridComponent } from './presentation/rooms/room-grid/room-grid.component';
+import { AuthComponent } from './presentation/auth/auth/auth.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: OverviewComponent },
-    { path: 'events', component: EventListComponent },
-    { path: 'rooms', component: RoomGridComponent },
-    // Add other routes as they get implemented
-    { path: '**', redirectTo: '/dashboard' } // Fallback route
-  ];
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'admin', component: OverviewComponent},
+  {path: 'client', component: OverviewComponent},
+  {path: 'auth', component: AuthComponent}, 
+  { path: 'events', component: EventListComponent },
+  { path: 'rooms', component: RoomGridComponent },
+  // Autres routes
+
+  { path: '**', redirectTo: '/not-found' }, // Fallback route
+];
